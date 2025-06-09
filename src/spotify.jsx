541b7aa22a4340f7f2512d2b12fdb56f77593bc2
spnandalskar <shwetanandalskar@gmail.com>
2025-06-09 19:57:58 +0530

    removed token

--- src/spotify.jsx
+++ src/spotify.jsx
@@ -1,11 +1,9 @@
 const CLIENT_ID = "daf660008efc4d4fadcca763ba4640c5";
 const REDIRECT_URI = "https://code-alpha-music-player-app.vercel.app/";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-const RESPONSE_TYPE = "token";
+
 const SCOPES = "user-library-read playlist-read-private";
 
 export const authUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
   REDIRECT_URI
-)}&scope=${encodeURIComponent(
-  SCOPES
-)}&response_type=${RESPONSE_TYPE}&show_dialog=true`;
+)}&scope=${encodeURIComponent(SCOPES)}&response_type=token&show_dialog=true`;
