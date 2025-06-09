d55d9fa64816f136090b1e522880dd3201b80744
spnandalskar <shwetanandalskar@gmail.com>
2025-06-09 20:10:06 +0530

    changed spotify.jsx

--- src/spotify.jsx
+++ src/spotify.jsx
@@ -2,8 +2,10 @@ const CLIENT_ID = "daf660008efc4d4fadcca763ba4640c5";
 const REDIRECT_URI = "https://code-alpha-music-player-app.vercel.app/";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 
-const SCOPES = "user-library-read playlist-read-private";
+const SCOPES = ["user-library-read", "playlist-read-private"];
 
 export const authUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
   REDIRECT_URI
-)}&scope=${encodeURIComponent(SCOPES)}&response_type=token&show_dialog=true`;
+)}&scope=${encodeURIComponent(
+  SCOPES.join("%20")
+)}&response_type=token&show_dialog=true`;
